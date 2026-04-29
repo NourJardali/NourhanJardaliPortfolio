67838095dbfdd353f510ccf84d9f50d83f90b158
Nourjardali <nourhan.jardali@outlook.com>
2026-04-29 15:16:33 +0300

    hero section updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import '../assets/styles/Main.scss';
+import nourhanImage from '../assets/images/profile.png';
 
 function Main() {
 
@@ -9,18 +11,20 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={nourhanImage} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            {/* <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a> */}
+            <a href="https://instagram.com/yourusername" target="_blank" rel="noreferrer"><InstagramIcon /></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Nourhan Jardali</h1>
+          <p>Senior Full Stack Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            {/* <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a> */}
+            <a href="https://instagram.com/yourusername" target="_blank" rel="noreferrer"><InstagramIcon /></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
